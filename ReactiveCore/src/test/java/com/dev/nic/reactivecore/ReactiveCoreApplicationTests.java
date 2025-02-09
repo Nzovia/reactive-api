b4b3ad4fba4f1f3fb005b051e0dd04c27c65674e
Nzovia <nickiemaundu19@gmail.com>
2025-02-09 23:23:37 +0300

    Code update

---
+++ ReactiveCore/src/test/java/com/dev/nic/reactivecore/ReactiveCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.nic.reactivecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
