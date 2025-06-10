8532f9530ff3b4fff78b60a01f6fa91cdd66853c
nmaundu <nmaundu@safaricom.co.ke>
2025-06-10 12:52:06 +0300

    Reactive core module

---
+++ reactiveCore/src/main/java/com/nicholas/reactivecore/ReactiveCoreApplication.java
@@ -0,0 +1,13 @@
+package com.nicholas.reactivecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveCoreApplication.class, args);
+    }
+
+}
